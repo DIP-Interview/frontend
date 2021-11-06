eba91a9357d7a3d086cade1ae56bf4e67c0d4ad3
Knoa0405 <mike1124@naver.com>
2021-11-06 21:42:06 +0900

    Co-authored-by: Hyewon <hyewonji@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
